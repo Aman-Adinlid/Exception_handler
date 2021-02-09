70cb2e1b0cd44eb2a11cf64f224f6814361a7d54
aman adinlid <95pinkpanda@gmail.com>
2021-02-09 15:44:36 +0100

    ............

---
+++ src/main/java/se/lexicon/Model/CheckingAccount.java
@@ -0,0 +1,11 @@
+package se.lexicon.Model;
+
+public class CheckingAccount {
+
+    private double balance;
+    private int number;
+
+    public CheckingAccount(int number) {
+        this.number = number;
+    }
+}
